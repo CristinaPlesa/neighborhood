595642a03224064a2cf0f8b545a4ca8426a9e68a
Cristina Plesa <cristinaplesa8@gmail.com>
2021-04-02 16:44:23 -0700

    Third test passed, but only if 0 is passed in.

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ function beepBoop(text) {
   if (text.trim().length === 0) {
     return "Please enter a number to get a response";
   }
-  if (text = 0)
-    for(let index = 0; index <= text.length; index +=1) {
+  if (text === "0")
+    for(let index = 0; index < text.length; index +=1) {
       return "Hello, this is the start!"
     }
   const justNumbers = text.replace(/[\D]/g, "");
