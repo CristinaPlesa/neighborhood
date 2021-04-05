b9371692e8fef8f1a86e3a00321d234784ec0d67
Cristina Plesa <cristinaplesa8@gmail.com>
2021-04-04 22:40:46 -0700

    WIP: Simplify and debug tests for numbers 0, 3, and 2.

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,21 @@ function beepBoop(text) {
   if (text.trim().length === 0) {
     return "Please enter a number to get a response";
   }
-  if (text === "0")
-    for(let index = 0; index < text.length; index +=1) {
-      return "Hello, this is the start!"
-    }
-  if (text === "3")
-    for(let index = 0; index < text.length; index +=1) {
-      return "Won't you be my neighbor"
-    }
-  if (text === "2")
-    for(let index = 0; index < text.length; index +=1) {
-      return "Boop"
-    }
   const justNumbers = text.replace(/[\D]/g, "");
   return justNumbers;
 }
 
+// for(let index = 0; index < text.length; index +=1)
+
+if (text === "0") {
+  return "Hello, this is the start!"
+}
+if (text === "3") {
+  return "Won't you be my neighbor"
+}
+if (text === "2") {
+  return "Boop"
+}
+
+// I also want the function to loop through each number up to the input, parseInt is needed to turn the string into number.
 // return parseInt(text, [0-9])
